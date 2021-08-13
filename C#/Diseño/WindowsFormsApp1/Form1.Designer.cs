7de9cbdae707ac79e557c34e971923ab303679f4
Arnol Veliz <arnolveliz10@gmail.com>
2021-08-13 14:16:43 -0600

    se agrego form de ingreso de usuarios

--- C#/Diseño/WindowsFormsApp1/Form1.Designer.cs
+++ C#/Diseño/WindowsFormsApp1/Form1.Designer.cs
@@ -37,6 +37,7 @@ namespace WindowsFormsApp1
             this.button1 = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
@@ -88,7 +89,7 @@ namespace WindowsFormsApp1
             this.button1.BackColor = System.Drawing.Color.Gold;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.Black;
-            this.button1.Location = new System.Drawing.Point(175, 249);
+            this.button1.Location = new System.Drawing.Point(197, 249);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(118, 48);
             this.button1.TabIndex = 4;
@@ -113,12 +114,25 @@ namespace WindowsFormsApp1
             this.button2.BackColor = System.Drawing.Color.Gold;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.ForeColor = System.Drawing.Color.Black;
-            this.button2.Location = new System.Drawing.Point(372, 249);
+            this.button2.Location = new System.Drawing.Point(392, 249);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(118, 48);
             this.button2.TabIndex = 8;
             this.button2.Text = "Salir";
             this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Gold;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.Black;
+            this.button3.Location = new System.Drawing.Point(254, 317);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(193, 35);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Agregar un nuevo usuario";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
@@ -127,6 +141,7 @@ namespace WindowsFormsApp1
             this.BackColor = System.Drawing.Color.LightSeaGreen;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(678, 458);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.button1);
@@ -154,6 +169,7 @@ namespace WindowsFormsApp1
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
